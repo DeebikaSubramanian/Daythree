15e9fa84525eba10dcee44e7cf0b9efa3bc495ff
DeebikaSubramanian <97935454+DeebikaSubramanian@users.noreply.github.com>
2022-01-30 20:32:33 +0300

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,7 @@
+var resume=[{"Name":"Deebika","Age":"33",
+            "Add":{"city":"Trichy","state":"Tamilnadu"},
+            "email":"deebideebikask@gmail.com",
+            "Year_of_Passing":"2012",
+            "Percentage_of_Mark":"83"
+                    }]
+                    console.log(resume)
